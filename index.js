be11236cf268305b8048d544fe8e972cb26da79f
Henry <henry@Henryde-iMac.local>
2019-09-26 15:25:34 +0800

    Create displayDataList function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
     const INDEX_URL = BASE_URL + '/api/v1/movies/'
     const POSTER_URL = BASE_URL + '/posters/'
     const data = []
+    const dataPanel = document.getElementById('data-panel')
 
     axios.get(INDEX_URL)
         .then((response) => {
@@ -11,4 +12,19 @@
             console.log(data);
         })
         .catch((err) => console.log(err))
+
+    function displayDataList(data) {
+        let htmlContent = ''
+
+        data.forEach(function(item, index) {
+            htmlContent += `
+                <div>s
+                    <img src="${POSTER_URL}${item.image}">
+                    <h6>${item.title}</h6>
+                </div>
+            `
+        })
+
+        dataPanel.innerHTML = htmlContent
+    }
 })()
